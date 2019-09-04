b54c5ebc977b2ec4da549f4234d698584e4b6485
Daniel Shokri <danielshokri@gmail.com>
2019-09-04 13:00:43 +0300

    Updates

---
+++ precache-manifest.d75226f611d4ae133b6fe7fc53d11f05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b112a026502244924ba92d12b8b37325",
+    "url": "/habibi-RESTAURANT/index.html"
+  },
+  {
+    "revision": "f9e5176080b079d04d81",
+    "url": "/habibi-RESTAURANT/static/css/2.a562377a.chunk.css"
+  },
+  {
+    "revision": "652451e918be5ead0c91",
+    "url": "/habibi-RESTAURANT/static/css/main.6280d28a.chunk.css"
+  },
+  {
+    "revision": "f9e5176080b079d04d81",
+    "url": "/habibi-RESTAURANT/static/js/2.a9630d1e.chunk.js"
+  },
+  {
+    "revision": "652451e918be5ead0c91",
+    "url": "/habibi-RESTAURANT/static/js/main.13100cfc.chunk.js"
+  },
+  {
+    "revision": "075487dc12a012c86970",
+    "url": "/habibi-RESTAURANT/static/js/runtime~main.78286e23.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/habibi-RESTAURANT/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/habibi-RESTAURANT/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/habibi-RESTAURANT/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/habibi-RESTAURANT/static/media/slick.f97e3bbf.svg"
+  }
+]);
